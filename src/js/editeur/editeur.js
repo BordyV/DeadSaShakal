407cee09b14ca6366d50460662a4b2b475792057
Birger <mathieubirger@gmail.com>
2019-11-14 15:49:14 +0100

    On peut placer des objets et le clique droit est capturé
    Pour placer différents objets : pavent numérique 1 et 2
    Clique droit capturé

--- src/js/editeur/editeur.js
+++ src/js/editeur/editeur.js
@@ -1,10 +1,9 @@
 var canvas, ctx, width, height;
-var char1, arbre;
+var objTemp, arbre;
 let objets=[];
-let map =[];
+let mapTemp =[];
 let object = new Object(0,0,0);
-console.log(object);
-let objet = {x:0, y:0, orientation :0, taille : 1, id : 0};
+let objet = {x:0, y:0, orientation :0, tailleX : 1, tailleY : 1, id : 0};
 let objetSelectione = 0;
 var mousepos = { x: 0, y: 0 };
 var inputStates = {};
@@ -19,34 +18,56 @@ function init() {
 
     // dernier param = temps min entre tirs consecutifs. Mettre à 0 pour cadence max
     // 500 = 2 tirs max par seconde, 100 = 10 tirs/seconde
-    char1 = new Mur(100, 100);
-    arbre = new Caisse(100,100);
+    objTemp = new Mur(100, 100);
 
     canvas.addEventListener('mousemove', function (evt) {
         mousepos = getMousePos(canvas, evt);
     }, false);
 
+    window.addEventListener('contextmenu', function(ev) {
+        ev.preventDefault();
+        return false;
+    }, false);
     window.addEventListener('click', function (evt) {
-        // on passe le temps en parametres, en millisecondes
-
-        // NOTE : si tu n'utilises pas inputStates.MOUSEDOWN
-        // ici, mais juste l'évébement click au lieu de mousedown
-        // tu ne pourras pas tirer plus vite, il te faudra
-        // marteler le bouton.
-        // compare en gardant space appuyé avec la cadence de
-        // tir à zero.
+        if(evt.button === 0) {
+            objet.id = objetSelectione;
+            objet.orientation = 0;
+            objet.taille = 1;
+            objet.x = mousepos.x;
+            objet.y = mousepos.y;
+            switch (objetSelectione) {
+                case 0:
+                    mapTemp.push(new Mur(mousepos.x, mousepos.y));
+                    break;
+                case 1:
+                    mapTemp.push(new Caisse(mousepos.x, mousepos.y));
+                    break;
+            }
+            //mapTemp.push(Mur(mousepos.x,mousepos.y));
+            console.log("objet: " + mapTemp);
+        }
     });
 
     window.addEventListener('keyup', function(evt) {
        // let key = evt.keyCode;
         if (evt.keyCode === 97) { // 1
-            char1 = new Mur(mousepos.x, mousepos.y);
+            objTemp = new Mur(mousepos.x, mousepos.y);
             objetSelectione = 0;
          //   console.log("Oui"+evt.keyCode);
         }
+
         if (evt.keyCode === 98) { // 1
+            objTemp = new Caisse(mousepos.x, mousepos.y);
             objetSelectione = 1;
             //   console.log("Oui"+evt.keyCode);
+        }
+
+        switch (evt.keyCode) {
+            case 97:
+                // to do
+                break;
+            case 98:
+                // tp dp
         }
     });
 
@@ -62,19 +83,12 @@ function anime() {
 
     // 1) On efface l'Ã©cran
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+        objTemp.draw(ctx);
+        objTemp.move(mousepos);
+    mapTemp.map(c => {
+        c.draw(ctx);
+    });
 
-
-    // 2) On dessine et on déplace le char 1
-    if( objetSelectione ===0)
-    {
-        char1.draw(ctx);
-        char1.move(mousepos);
-    }
-    if( objetSelectione ===1)
-    {
-        arbre.draw(ctx);
-        arbre.move(mousepos);
-    }
     if(inputStates) {
         //char1.addBullet(Date.now());
     }
--- src/js/model/Caisse.js
+++ src/js/model/Caisse.js
@@ -1,13 +1,16 @@
 class Caisse extends Objet {
+    tailleX =40;
+    tailleY = 50;
     draw(ctx) {
         ctx.save();
         ctx.translate(this.x, this.y);
-        ctx.translate(-10, -10);
+       // ctx.translate(-1*this.tailleX/2, -1*this.tailleY/2);
 
         // corps
         ctx.fillStyle = "red";
         ctx.color = "red";
-        ctx.fillRect(0, 0, 20, 20);
+        ctx.rotate(157);
+        ctx.fillRect(0, 0, this.tailleX, this.tailleY);
         // canon
 
 
