0c0a0eb711c248176e27f10ddb5db2511b8ed496
valentin <valentin.bordy@homtail.com>
2019-12-29 12:55:01 +0100

    continuite mur Triangle
    continuité mur Triangle.

--- src/js/model/TriangleRect.js
+++ src/js/model/TriangleRect.js
@@ -2,10 +2,41 @@ class TriangleRect extends Object {
     constructor(id, x,y, height, width) {
 
 
-        super(id, x,y,height, width);
-    
-    
+      super(id, x,y,height, width);
+      this.id=id;
+      this.width = width;
+      this.height = height;
+      if(this.id == 201)
+      {
+      this.image = document.getElementById("angle1");
+      }
 
-        
+  }
+
+    drawObj(ctx) {
+      ctx.save();
+      ctx.translate(this.x, this.y);
+      ctx.shadowBlur = 15;
+      ctx.shadowColor = "black";
+      if(this.surbrillance) {
+        ctx.shadowBlur = 20;
+        ctx.shadowColor = "white";
+      }
+
+      if(this.id == 201)
+      {
+        ctx.beginPath();
+        ctx.moveTo(0, 0);
+          ctx.lineTo(this.width, 0);
+          ctx.lineTo(0, this.height);
+          ctx.fill();
+      }
+
+      ctx.drawImage(this.image, 0, 0,this.width,this.height);
+      
+
+
+      ctx.restore();
+  }
     }
 }
